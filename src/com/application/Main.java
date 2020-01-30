04aeae19bec94ab7a7505ada03c5713367687e10
Darren-B312 <Darren-B312@Outlook.com>
2020-01-30 11:36:41 +0000

    added server functionality for connecting to clients and hosting games

--- src/com/application/Main.java
+++
@@ -1,8 +0,0 @@
-package com.application;
-
-public class Main {
-	
-	public static void main(String[] args) {
-		
-	}
-}
--- src/com/networking/ConnectionListener.java
+++ src/com/networking/ConnectionListener.java
@@ -3,19 +3,38 @@ package com.networking;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Queue;
 
 public class ConnectionListener implements Runnable{
 
-	private ServerSocket listener;
+	private final int PORT = 10000;
+	private final int BACKLOG = 10;
 	
-	public ConnectionListener(int port, int backlog) throws IOException {
-		listener = new ServerSocket(port, backlog);
+	private Queue<Client> clients;
+	private ServerSocket listener;
+	private int clientID = 0;
+	
+	public ConnectionListener(Queue<Client> clients) {
+		this.clients = clients;
+		System.out.println("[Server]> spawning connection listener thread...");
 	}
-
+	
 	@Override
-	public void run() {
-		while(true) {
-			//Socket connection = listener.accept();
+	public void run() {	
+		try {
+			listener = new ServerSocket(PORT, BACKLOG);
+			System.out.println("[Server]> listening for new connections...");
+			while(true) {
+				Socket connection = listener.accept();			
+				clientID++;
+				
+				clients.add(new Client(connection, clientID));
+				System.out.println("[Server]> new client #" + clientID + ", connected from ip:" + connection.getInetAddress());
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
+
 }
