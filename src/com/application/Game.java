c79f70674265fa67532811c076ed2f830b53688f
Darren-B312 <Darren-B312@Outlook.com>
2020-01-31 18:42:59 +0000

    commit cleanup

--- src/com/application/Game.java
+++ src/com/application/Game.java
@@ -15,14 +15,14 @@ public class Game {
 
 		final InetAddress IP = InetAddress.getByName("127.0.0.1");
 		final int PORT = 10000;
-		
+
 		Scanner console = new Scanner(System.in);
-		
+
 		Connection connection = new Connection(IP, PORT);
 		Thread client = new Thread(connection);
-		
-		client.start();	
-		
+
+		client.start();
+
 	}
 
 }
