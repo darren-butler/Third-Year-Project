c476aebd96898702a9ae6c57060d9ccbd07e2c8d
Darren Butler <57804177+Darren-B312@users.noreply.github.com>
2020-02-24 14:52:39 +0000

    Revert "Develop"

---
+++ src/com/graphics/ActionType.java
@@ -0,0 +1,7 @@
+package com.graphics;
+
+public enum ActionType {
+	//A Number of actions a piece can take.
+	NONE, MOVE, ATTACK
+	
+}
---
+++ src/com/networking/Server.java
@@ -0,0 +1,25 @@
+package com.networking;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Server {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		String str = "this is the starting game-state";
+
+		Queue<Client> clients = new LinkedList<Client>();
+		new Thread(new ConnectionListener(clients)).start();
+
+		while (true) {
+			Thread.sleep(1000);
+			System.out.println("[Server]> " + clients.size() + " clients in queue");
+			if (clients.size() >= 2) {
+				new Thread(new GameInstance(str, clients.poll(), clients.poll())).start();
+			}
+		}
+
+	}
+
+}
