f0f9c450d74717771b3c879445e5813c2f949a91
Darren <Darren@Darren-B312@Outlook.com>
2020-04-12 13:12:40 +0100

    did a thing?

--- src/com/graphics/GameController.java
+++ src/com/graphics/GameController.java
@@ -15,36 +15,62 @@ public class GameController extends JFrame {
 
 	private Cell[][] board;
 	private Canvas canvas;
+	private Cell player = Cell.X;
+	private boolean turn = true;
 	
 	public GameController() {
-		
+
 		board = new Cell[Utilities.ROWS][Utilities.COLS];
 		initGame();
-		
+
 		canvas = new Canvas(board);
 		canvas.setPreferredSize(new Dimension(Utilities.CANVAS_WIDTH, Utilities.CANVAS_HEIGHT));
-		
+
 		Container cp = getContentPane();
 		cp.setLayout(new BorderLayout());
 		cp.add(canvas, BorderLayout.CENTER);
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		pack();  // pack all the components in this JFrame
+		pack(); // pack all the components in this JFrame
 		setTitle("Tic Tac Toe");
-		setVisible(true);  // show this JFrame
+		setVisible(true); // show this JFrame
+
+		canvas.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) { // mouse-clicked handler
+
+				int mouseX = e.getX();
+				int mouseY = e.getY();
+				int rowSelected = mouseY / Utilities.CELL_SIZE;
+				int colSelected = mouseX / Utilities.CELL_SIZE;
+
+				while(turn) {
+					if (rowSelected >= 0 && rowSelected < Utilities.ROWS && colSelected >= 0 && colSelected < Utilities.COLS
+							&& board[rowSelected][colSelected] == Cell.EMPTY) {
+						board[rowSelected][colSelected] = player;
+						// update state
+						// Switch player
+						// currentPlayer = (currentPlayer == Seed.X) ? Seed.O : Seed.X;
+					}
+					repaint();
+					turn = !turn;
+				}
+
+			}
+		});
 		
 
 	}
 	
-	
+
 	private void initGame() {
 		for (int row = 0; row < Utilities.ROWS; ++row) {
-	         for (int col = 0; col < Utilities.COLS; ++col) {
-	            board[row][col] = Cell.EMPTY; // all cells empty
-	         }
-	      }
+			for (int col = 0; col < Utilities.COLS; ++col) {
+				board[row][col] = Cell.EMPTY; // all cells empty
+			}
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		GameController gc = new GameController();
 	}
