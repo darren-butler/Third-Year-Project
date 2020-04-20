e2b831cbb5cfcaabec4f10a11fe0fbaab5215c03
MrSkillage <conorrabbitte.it3@gmail.com>
2020-04-20 16:47:24 +0100

    Commented the Utilities class

--- src/com/graphics/Utilities.java
+++ src/com/graphics/Utilities.java
@@ -1,27 +1,28 @@
 package com.graphics;
-
+/*
+ * This class is used to store the games related utilities.  These are final variables that
+ * will never change during execution and are called multiple times throughout different classes.
+ * The purpose of this 'Utilities' class was to prevent redundant repetition of data.
+ */
 public class Utilities {
-
-	// Named-constants for the board dimensions
-	// Number of Board Rows.
+	// Number of board rows.
 	public static final int ROWS = 3;
-	// Number of Board Columns.
+	// Number of board columns.
 	public static final int COLS = 3;
-	// Cell width and height (square)
+	// Cell width and height (each square)
 	public static final int CELL_SIZE = 100;
-	// Drawing the canvas Width
+	// Canvas width for drawing graphics
 	public static final int CANVAS_WIDTH = CELL_SIZE * COLS;
-	// Drawing the canvas Height
+	// Canvas height for drawing graphics
 	public static final int CANVAS_HEIGHT = CELL_SIZE * ROWS;
-	// Grid-lines width
+	// Grid-lines width for drawing graphics
 	public static final int GRID_WIDTH = 8;
-	// Grid-line's half-width
+	// Grid-line's half-width for drawing graphics
 	public static final int GRID_WIDTH_HALF = GRID_WIDTH / 2;
-	// Symbols (X/O) are displayed inside a cell, with padding from border
+	// Symbols (X/O) are displayed inside a cell, with padding from border using this dimension
 	public static final int CELL_PADDING = CELL_SIZE /6;
-	// Width / Height
+	// Width and height of the players symbol (X/O) in proportion to the cell
 	public static final int SYMBOL_SIZE = CELL_SIZE - CELL_PADDING *2;
-	// Pen's stroke width
+	// Pen's (symbol) stroke width for drawing graphics
 	public static final int SYMBOL_STROKE_WIDTH = 8;
-	
 }
