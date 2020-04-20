3398702a535a6bb9747f43160cd2ffd5e0863baf
MrSkillage <conorrabbitte.it3@gmail.com>
2020-04-20 15:34:04 +0100

    Commented Cell and GameState classes

--- src/com/graphics/Cell.java
+++ src/com/graphics/Cell.java
@@ -1,6 +1,10 @@
 package com.graphics;
-
+/*
+ * This enumeration class is used to store cell information about the board.
+ * The board will be composed of a 2D Array of type Cell with each Array address
+ * pointing to either EMPTY, X, or O.
+ */
 public enum Cell {
-	// Enumerations for the seeds and cells contents.
+	// The 3 enumerations (States) of Cell
 	EMPTY, X, O
 }
--- src/com/graphics/GameState.java
+++ src/com/graphics/GameState.java
@@ -1,6 +1,12 @@
 package com.graphics;
-
+/*
+ * This enumeration class is used to store the Games State.  This class will be
+ * used to check the current state of the game and its win conditions.
+ * This will be used in methods relating to updating the running a games state.
+ * Each game will have only 1 state running at a time which could either be PLAYING, DRAW
+ * X_WON, or O_WON
+ */
 public enum GameState {
-	// Enumerations for the various states of the game.
+	// The 4 enumerations (States) of the Game State
 	PLAYING, DRAW, X_WON, O_WON
 }
